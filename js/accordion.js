1ee788cc0293191e2d177a0dd96ab5a05f5d3682
mmelamm <145041958+mmelamm@users.noreply.github.com>
2024-05-17 12:45:57 +0100

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,33 @@
+var acc = document.querySelectorAll(".accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      this.classList.remove("active");
+      panel.style.display = "none";
+    } else {
+      this.classList.add("active"); 
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+var acc = document.querySelectorAll(".accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
